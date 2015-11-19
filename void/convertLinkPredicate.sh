426d72ad7e7c6f0232f184706e851b5807130a5e
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-11-19 17:26:52 +0000

    updated void for uniprot 2015_11
    From https://github.com/openphacts/ops-platform-setup/tree/2.0.0/void/linksets

--- void/convertLinkPredicate.sh
+++
@@ -1,8 +0,0 @@
-
-for file in *.ttl
-do
-	sed 's/rdfs:seeAlso/<http:\/\/semanticscience.org\/resource\/SIO_010043>/g' < $file > $file.tmp
-	sed 's/skos:exactMatch/rdfs:seeAlso/g' < $file.tmp > $file
-	rm $file.tmp
-done
-
