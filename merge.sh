17ac6cfe36bac5c904d2217a885aecc83246e66a
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-11-19 17:29:49 +0000

    _ls_void filenames

--- merge.sh
+++ merge.sh
@@ -2,7 +2,7 @@
 mkdir -p merged
 rm -f merged/*
 for void in void/*.ttl ; do
-  name=`basename $void`
+  name=`basename $void | sed s/_ls_void//`
   data=data/$name.gz
   merged=merged/$name  
   cp $void $merged
